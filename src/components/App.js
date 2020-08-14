fc8a3f5155d49e07e8e65822342e5c7a5a546161
Brandt <breutiman@gmail.com>
2020-08-13 20:13:29 -0700

    React App frontend and dummy (non interactive app created)

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,46 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 import '../styles/App.css';
-import Content from './Content'
+import '../styles/Content.css'
 import TopBar from './TopBar'
 import SideBar from './SideBar'
+import CardGrid from './CardGrid';
+import Pagination from './Pagination';
+import ColorDetail from './ColorDetail';
+
+const colors = [
+  "#112233", "#551133", "#112266", "#44FF22",
+  "#112233", "#551133", "#112266"
+]
 
 function App() {
   return (
-    <div className="App">
-      <TopBar />
-      <SideBar />
-      <Content />
-    </div>
+    <Router>
+      <div className="App">
+        <TopBar />
+        <main>
+          <SideBar />
+          <div className="Content">
+            <Switch>
+              <Route exact path='/'>
+                <CardGrid colors={ colors }></CardGrid>
+                <Pagination />
+              </Route>
+              <Route path='/color-detail/:color'>
+                <ColorDetail />
+              </Route>
+              <Route>
+                <p>Page Not Found</p>
+              </Route>
+            </Switch>
+          </div>
+        </main>
+      </div>
+    </Router>
   );
 }
 
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import Button from './Button.js';
+import '../styles/SideBar.css';
 
 function SideBar(props) {
     return (
-        <div>
-            <span>I am the sidebar</span>
+        <div className="SideBar" data-testid="SideBar">
+            <Button value="Random Color" width="100%"></Button>
+            <ul>
+                <li>Red</li>
+                <li>Green</li>
+                <li>Blue</li>
+                <li>Orange</li>
+            </ul>
         </div>
     )
 }
